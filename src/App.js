c68f10e1b8560ed73c8d0ab7bc10a0658052e5d3
JigneshMahadik <jigneshmahadik2898@gmail.com>
2024-04-20 13:43:15 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
+import jsonData from "./paragraph.json"
 
 function App() {
+
+  const [ state, setState ] = useState(0);
+
+  function input() {
+    const input = document.getElementById("input").value;
+    if(input>0 && input<=7){
+      setState(parseInt(input));
+    }
+    else if(input>7){
+      alert("Ayyo ! you are demanding very much paragraph in one go, kindly take little litlle 😀");
+      setState(0);
+    }
+    else if(input<=0){
+      alert("Ayyo ! you know that you wrote negative input 😵");
+      setState(0);
+    }
+    // alert(input);
+  }
+  // const btnGen = ()=>{
+  //   for(let i=1;i<=state;i++){
+  //     console.log(i);
+  //   }
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TIRED OF BORING LOREM IPSUM?</h1>
+      <label>Paragraph : </label>
+      <input type="number" id="input"/>
+      <button onClick={input}>GENERATE</button>
+      <div>
+        {
+          jsonData.paragraphs.slice(0,state).map((par,id)=>{
+            return(
+              <div>
+                <br/><br/>
+                <span>{id+1})</span>
+                <span key={id}>{par.para}</span>
+                <br/><br/>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
